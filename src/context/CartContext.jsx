8039d0a30b83a39063aa69f16bef60523c5e13da
unknown <leblancmichael94@gmail.com>
2025-11-13 17:30:47 -0500

    completed context page and fixed home page for cart feature

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,1 +1,56 @@
 import React, { createContext, useContext, useState } from 'react';
+
+// Creates the shared "cart box" that components will read from
+const CartContext = createContext(null);
+
+export const CartProvider = ({ children }) => {
+
+    // The cart state that will be shared across the app
+    const [items, setItems] = useState([]);
+
+    // Function to update the shared cart
+    const addToCart = (product) => {
+        setItems((prevItems) => {
+            // Check if the product is already in the cart (returns true or false)
+            const existing = prevItems.find((item) => item.id === product.id);
+
+            if (existing) {
+                // If item already in the cart, increase the quantity
+                return prevItems.map((item) => 
+                item.id === product.id
+                ? {...item, qty: item.qty + 1 }
+                : item
+                );
+            }
+
+            // If its a new product, add it with a quantity of 1
+            return [
+                ...prevItems,
+                {
+                    id: product.id,
+                    title: product.title,
+                    price: Number(product.price),
+                    image: product.images?.[0] || product.thumbnail || '',
+                    qty: 1,
+                },
+            ];
+        });
+    }
+
+    // The shared data/function we expose to the whole app
+    const value = { items, addToCart };
+
+    // Wrap the entire app with the provider so all components get access
+    return (
+        <CartContext.Provider value={value}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+// Hook to let any component read/use the cart
+export const useCart = () => {
+    const ctx = useContext(CartContext);
+    if (!ctx) throw new Error('useCart must be used within a CartProvider');
+    return ctx;
+}
