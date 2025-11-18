7378ea7aa97e76554751a84596a0a55b28cb69b6
unknown <leblancmichael94@gmail.com>
2025-11-18 12:44:27 -0500

    simplified the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
+      <Header /> 
       <Outlet />
-      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-import './main.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import NotFound from './pages/NotFound.jsx';
+import Home from './pages/Home.jsx';
+import Cart from './pages/Cart.jsx';
+import { CartProvider } from './context/CartContext.jsx';
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                index: true, 
+                element: <Home />
+            }, 
+            {
+                path: '/cart',
+                element: <Cart />
+            }
+        ]
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <CartProvider>
+    <RouterProvider router={router} />
+    </CartProvider>
+)
